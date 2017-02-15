18e1cb89d48281b7d39ea220e56c044a09669968
Eduardo Cabrera <ecabrera@urbaner.pe>
2017-02-15 01:20:23 -0500

    Port for server listen has been changed. Console logs have been moved to /

--- app.js
+++ app.js
@@ -11,14 +11,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function(req,res){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.send("Luu App! I'm rat kid");
+  res.send("Mi primera APP con Jenny");
+
+  console.log('Corriendo en el puerto 3000');
+  console.log('Un 14 Feb diferente'); // Puedes borrarlo o comentarlo.
 });
 
-// Create an HTTP service.
-//server.listen(3000);
-app.listen(3000, function () {
+// Create an HTTP service as process.
+server.listen(9000);
+
+/*app.listen(9000, function () {
   console.log('Corriendo en el puerto 3000');
-});
+  // Puedes borrarlo o comentarlo.
+  console.log('Un 14 Feb diferente');
+});*/
 
 app.get("/mail", function(req, res){
 	res.send('mailing!');
