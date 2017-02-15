733102560c510d3bbdb5c45e59179c46de38192f
Eduardo Cabrera <ecabrera@urbaner.pe>
2017-02-15 00:30:38 -0500

    Initial commit. Use npm install for Express and Body-Parse. The last is for request with application/json and x-www-form-urlencoded Headers

---
+++ app.js
@@ -0,0 +1,22 @@
+var express 	= require("express");
+var http        = require("http");
+var app         = express();
+var bodyParser  = require('body-parser');
+
+server       = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req,res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send("Luu App! I'm rat kid");
+});
+
+// Create an HTTP service.
+server.listen(3000);
+
+app.get("/mail", function(req, res){
+	res.send('mailing!');
+})
